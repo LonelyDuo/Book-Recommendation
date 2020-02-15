f3fc0e42ce873ce65940bc1aad81b711c1c98bd8
Jeffrey Huang <GamerJeffreyYT@users.noreply.github.com>
2020-02-15 12:56:26 -0500

    Testing Stuff

---
+++ Recom.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+#Make the actual request to the API using the python requests library
+#You may need to run the command 'pip install requests' in your terminal
+book = input('pick book subject: ')
+api_request_response = requests.get(url="https://www.googleapis.com/books/v1/volumes?content-type=application/json&q="+book+"+subject&key=AIzaSyBlnhTDwJxnr9INvUAz4FMLO4dKoRBureU"
+)
+
+#Convert the data into a JSON object in python
+response_data = api_request_response.json()
+# print(response_data['items'][0]['volumeInfo']['averageRating'])
+print(response_data)
